57afa29e093cb93cd8619ed3aac9c0856830db3b
TimGels <43609220+TimGels@users.noreply.github.com>
2023-07-01 14:03:36 +0200

    Restructured to a more common structure
    Renamed Controller directory to Api.
    Moved the task to its own scheduledtasks directory.

--- Jellyfin.Plugin.Template/Services/JellybenchManager/JellybenchManagerService.cs
+++ Jellyfin.Plugin.Template/Services/JellybenchManager/JellybenchManagerService.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Jellyfin.Plugin.Template.ScheduledTasks;
 using MediaBrowser.Model.Tasks;
 
 namespace Jellyfin.Plugin.Template.Services.JellybenchManager;
